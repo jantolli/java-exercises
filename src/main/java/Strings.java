c5bf602c86eb258562ce83a3d407319913b3e10b
jantolli <joseantollini@gmail.com>
2021-03-05 17:13:08 -0600

    Simple exercise to remove repeated charasters from a string. 2 solutions provided. One using Set and another using lambda

---
+++ src/main/java/Strings.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Strings {
+
+    public static String getUniqueCharsUsingLambda(String str) {
+        String result = str.chars().mapToObj((int m) -> (char) m).distinct().map(o -> o.toString()).collect(Collectors.joining());
+        result = String.join("", result);
+        return result;
+    }
+
+    public static String getUniqueCharsUsingSet(String str) {
+
+        char[] chars = str.toCharArray();
+        Set<String> set = new HashSet<>();
+        for (char aChar : chars) {
+            set.add(String.valueOf(aChar));
+        }
+        return String.join("", set);
+    }
+
+    public static void main(String[] args) {
+        String stringWithRepeatedChars = "aaaalllllmmm";
+        System.out.println(getUniqueCharsUsingLambda(stringWithRepeatedChars));
+        System.out.println(getUniqueCharsUsingSet(stringWithRepeatedChars));
+    }
+
+}
---
+++ src/test/java/StringsTest.java
@@ -0,0 +1,16 @@
+import org.junit.jupiter.api.Assertions;
+
+class StringsTest {
+
+    @org.junit.jupiter.api.Test
+    void getUniqueCharsUsingLambda() {
+        String stringWithRepeatedChars = "aaaalllllmmm";
+        Assertions.assertEquals("alm", Strings.getUniqueCharsUsingLambda(stringWithRepeatedChars), "Values don't match");
+    }
+
+    @org.junit.jupiter.api.Test
+    void getUniqueCharsUsingSet() {
+        String stringWithRepeatedChars = "aaaalllllmmm";
+        Assertions.assertEquals("alm", Strings.getUniqueCharsUsingSet(stringWithRepeatedChars), "Values don't match");
+    }
+}
